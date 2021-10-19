dd73a066e79b6ed4fb68232f90dc02f87e4173c2
teamdeebo <teamdeebo@hotmail.com>
2021-10-18 20:22:13 -0500

    update

--- js/main.js
+++ js/main.js
@@ -10,16 +10,16 @@ function answerToggle(e){
     let status = e.target.parentElement;
     console.log(e.target);
     if(e.target.classList.contains('question')){
-    if(status.classList.contains('info--active')){
-        console.log(e.target);
-        status.classList.toggle('info--active');        
-    }else{
-        console.log(e.target);
-        let previous = document.querySelector('.info--active')
-        if(previous){
-            previous.classList.remove('info--active');
+        if(status.classList.contains('info--active')){
+            console.log(e.target);
+            status.classList.toggle('info--active');        
+        }else{
+            console.log(e.target);
+            let previous = document.querySelector('.info--active')
+            if(previous){
+                previous.classList.remove('info--active');
+        }
+            status.classList.toggle('info--active');        
+        }
     }
-        status.classList.toggle('info--active');        
-    }
-}
 }
